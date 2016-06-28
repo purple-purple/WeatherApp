80717233466fe7ddb5306ad30faaee1e8092b7cd
purple-purple <purple-purple@outlook.com>
2016-06-28 12:34:17 +0800

    完成最基本的天气数据获取

--- src/com/example/weatherapp/activity/WeatherActivity.java
+++ src/com/example/weatherapp/activity/WeatherActivity.java
@@ -1,14 +1,21 @@
 package com.example.weatherapp.activity;
 
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 import com.example.weatherapp.R;
-import com.example.weatherapp.util.HttpCallbackListener;
 import com.example.weatherapp.util.HttpUtil;
 import com.example.weatherapp.util.Weather_Util;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -26,6 +33,27 @@ public class WeatherActivity extends Activity implements OnClickListener {
 	private Weather_Util weather_util;
 	private HttpUtil http_util;
 	private String weather_response;
+	
+	public static final int SHOW_RESPONSE=0;
+	private String address;
+	private Handler handler=new Handler(){
+		public void handleMessage(Message msg){
+			switch(msg.what){
+			case SHOW_RESPONSE:
+				weather_response=(String) msg.obj;
+				//Log.d("my_response", weather_response);
+				Weather_Util.handle_Weather_Response(weather_response);
+				text_tem_min.setText(weather_util.tmp_min_today+"℃");
+				text_tem_max.setText(weather_util.tmp_max_today+"℃");
+				if(weather_util.txt_d.equals(weather_util.txt_n)){
+					text_weather_message.setText(weather_util.txt_d);
+				}
+				else{
+					text_weather_message.setText(weather_util.txt_d+" 转 "+weather_util.txt_n);
+				}
+			}
+		}
+	};
 	@Override
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
@@ -41,10 +69,13 @@ public class WeatherActivity extends Activity implements OnClickListener {
 		Bundle bundle=intent.getExtras();
 		text_city_name.setText(bundle.getString("city_name"));
 		
-		http_util=new HttpUtil();
-		String address="https://api.heweather.com/x3/weather?city=厦门&key=d3fd71a8023142f39f2a03b61e0d3df2";
-		Log.d("my_response", "1");
-		HttpUtil.sendHttpRequest(address, new HttpCallbackListener(){
+		//http_util=new HttpUtil();
+		address="https://api.heweather.com/x3/weather?city="
+				+
+				bundle.getString("city_name").substring(0, bundle.getString("city_name").length()-1)
+				+
+				"&key=d3fd71a8023142f39f2a03b61e0d3df2";
+		/*HttpUtil.sendHttpRequest(address, new HttpCallbackListener(){
 			@Override
 			public void onFinish(String response){
 				weather_response=response.toString();
@@ -54,10 +85,10 @@ public class WeatherActivity extends Activity implements OnClickListener {
 			public void onError(Exception e){
 				e.printStackTrace();
 			}
-		});
-		weather_util.handle_Weather_Response(this, weather_response);
+		});*/
+		sendRequest_Http_URLConnection();
 		//Log.d("my_response", weather_util.tmp_max_today);
-		text_tem_max.setText(weather_util.tmp_max_today);
+		
 	}
 	
 	@Override
@@ -66,4 +97,40 @@ public class WeatherActivity extends Activity implements OnClickListener {
 
 	}
 
+	private void sendRequest_Http_URLConnection(){
+		new Thread(new Runnable(){//开启子线程处理HTTP请求内容
+			@Override
+			public void run(){
+				HttpURLConnection connection=null;
+				try{
+					URL url=new URL(address);
+					//URL url=new URL("http://www.baidu.com");
+					connection=(HttpURLConnection) url.openConnection();//获得连接实例
+					connection.setRequestMethod("GET");//设置请求方法 GET/POST
+					connection.setConnectTimeout(80000);//连接超时
+					connection.setReadTimeout(80000);//读取超时
+					InputStream in=connection.getInputStream();//获取服务器返回的输入流
+					BufferedReader reader=new BufferedReader(new InputStreamReader(in));//输入流文本容器
+					StringBuilder response=new StringBuilder();//字符串容器
+					String line;
+					while((line=reader.readLine())!=null){
+						response.append(line);//添加内容	
+						Log.d("my_response", "success");
+					}
+					Message message=new Message();
+					message.what=SHOW_RESPONSE;
+					message.obj=response.toString();
+					handler.sendMessage(message);//发送信息
+						
+				}catch(Exception e){
+					e.printStackTrace();
+				}finally{
+					if(connection!=null){
+						connection.disconnect();//关闭HTTP连接
+					}
+				}
+			}
+		}).start();
+	}
+	
 }
--- src/com/example/weatherapp/util/Weather_Util.java
+++ src/com/example/weatherapp/util/Weather_Util.java
@@ -4,6 +4,7 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import android.content.Context;
+import android.util.Log;
 
 public class Weather_Util {
 	public static String data_today;
@@ -12,19 +13,26 @@ public class Weather_Util {
 	public static String txt_d;
 	public static String txt_n;
 	
-	public static void handle_Weather_Response(Context context,String response){
+	public static void handle_Weather_Response(String response){
 		try{
+			Log.d("my_response", "handle begin");
 			JSONObject jsonObject=new JSONObject(response);
-			JSONObject all_information_obj=jsonObject.getJSONObject("HeWeather data service 3.0");
-			String all_information_str=all_information_obj.toString();
-			JSONArray  jsonArray=new JSONArray(all_information_str);
-			JSONObject weather_inf=jsonArray.getJSONObject(2);
-			data_today=weather_inf.getString("data");
-			tmp_max_today=weather_inf.getJSONObject("tmp").getString("max");
-			tmp_min_today=weather_inf.getJSONObject("tmp").getString("min");
-			txt_d=weather_inf.getJSONObject("cond").getString("txt_d");
-			txt_n=weather_inf.getJSONObject("cond").getString("txd_n");
-			
+			JSONArray all_information_arry=jsonObject.getJSONArray("HeWeather data service 3.0");
+			//String all_information_str=all_information_arry.toString();
+			//Log.d("my_response",all_information_str);
+			//JSONArray  jsonArray=new JSONArray(all_information_str);
+			JSONObject weather_inf=all_information_arry.getJSONObject(0);			
+			//Log.d("my_response",weather_inf.toString());
+			//Log.d("my_response","1");
+			JSONObject daily_forecast=weather_inf.getJSONArray("daily_forecast").getJSONObject(0);
+			data_today=daily_forecast.getString("date");
+			//Log.d("my_response", data_today);
+			tmp_max_today=daily_forecast.getJSONObject("tmp").getString("max");
+			tmp_min_today=daily_forecast.getJSONObject("tmp").getString("min");
+			//Log.d("my_response", tmp_max_today);
+			txt_d=daily_forecast.getJSONObject("cond").getString("txt_d");
+			txt_n=daily_forecast.getJSONObject("cond").getString("txt_n");
+			//Log.d("my_response", txt_d);
 			
 		}catch(Exception e){
 			e.printStackTrace();
